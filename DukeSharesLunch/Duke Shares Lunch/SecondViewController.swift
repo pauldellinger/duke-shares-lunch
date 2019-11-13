957cc51c670942d88446103b4f9958e183ebf8a2
Paul Dellinger <pd88@duke.edu>
2019-11-13 17:15:17 -0500

    testing the Xcode GitHub system

--- DukeSharesLunch/Duke Shares Lunch/SecondViewController.swift
+++ DukeSharesLunch/Duke Shares Lunch/SecondViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
