b47ac7a5931da123e70ee453ddce6fa358b31748
Paul Dellinger <pd88@duke.edu>
2019-11-21 22:44:23 -0500

    Made the user login page. Validation regex can be used for sign up as well

---
+++ DukeSharesLunch/Duke Shares Lunch/UserDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  UserDetailViewController.swift
+//  Duke Shares Lunch
+//
+//  Created by Chris Theodore on 11/21/19.
+//  Copyright © 2019 July Guys. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
