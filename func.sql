5b114ca003d4a2b790a760f16a803c438b8a2e7a
Paul Dellinger <pd88@duke.edu>
2019-11-30 16:25:09 -0500

    sql function for unapproved purchases given sellerid, changed primary key of activeseller to (uid, location) which will get rid of duplicates and allow upserting

---
+++ func.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION unapproved_purchase(sellerid integer)
+	RETURNS TABLE (
+	pid integer,
+	saleid integer,
+	price DECIMAL(5,2),
+	approve BOOLEAN,
+	paid BOOLEAN,
+	p_description VARCHAR,
+	buyername VARCHAR,
+	buyervenmo VARCHAR
+)
+AS $$
+	SELECT pid, purchase.saleid as saleid, price, approve,paid, p_description, 
+	buyer.name AS buyername,buyer.venmo as buyervenmo
+	FROM PURCHASE
+	LEFT JOIN REGISTEREDUSER as buyer
+	on PURCHASE.bid = buyer.uid
+	LEFT JOIN ACTIVESELLER
+	on PURCHASE.saleid = ACTIVESELLER.saleid
+	LEFT JOIN REGISTEREDUSER as seller
+	on ACTIVESELLER.uid = seller.uid
+	WHERE seller.uid = sellerid;
+$$
+LANGUAGE 'sql';
