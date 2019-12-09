5d85a5d96f1d0c83e2a728cac7f2e56b7597aecd
Paul Dellinger <pd88@duke.edu>
2019-12-08 23:36:27 -0500

    ordered active restaurants by count

--- build-db.sql
+++ build-db.sql
@@ -30,7 +30,7 @@ CREATE TABLE Purchase
                 price DECIMAL(5,2) CHECK(price > 0) NOT NULL,
                 approve BOOLEAN NOT NULL,
                 paid BOOLEAN NOT NULL,
-                p_description VARCHAR(200));
+                p_description VARCHAR(1000));
 
 
 CREATE TABLE Meals
@@ -45,6 +45,7 @@ CREATE TABLE Meals
 INSERT INTO RegisteredUser VALUES(1, 'pd88@duke.edu',  'Paul Dellinger', 'paul_dellinger', 'Computer Science', 'Kilgo');
 INSERT INTO RegisteredUser VALUES(2, 'jcr34@duke.edu',  'Josh Romine', 'joshielikescash');
 INSERT INTO RegisteredUser VALUES(3, 'aje11@duke.edu',  'AJ Eckmann', 'AJs Venmo');
+--INSERT INTO basic_auth.users VALUES('pd88@duke.edu', 'Password1', 'todo_user');
 --Three example users
 
 INSERT INTO ActiveSeller VALUES(2001, 1,'2019-10-25 02:36:00', TRUE, 0.60, 'Il Forno');
@@ -58,9 +59,9 @@ INSERT INTO SellPreferences VALUES(1, 'Ginger and Soy');
 --Paul Likes to sell his food points at il forno and ginger and soy
 --because he eats at those restaurants a lot
 
-INSERT INTO Purchase VALUES(3001, 2001, 2, 8.95, FALSE, FALSE, 'Hey paul can you get me rigatoni with pomodoro, spinach and extra chiken');
+--INSERT INTO Purchase VALUES(3001, 2001, 2, 8.95, FALSE, FALSE, 'Hey paul can you get me rigatoni with pomodoro, spinach and extra chiken');
 --Paul is going to buy something for Josh at il forno
---Jjosh will venmo Paul based on the price
+--Josh will venmo Paul based on the price
 
 INSERT INTO Meals VALUES(4001, 8.85, 'Il Forno', 'Make your own pasta bowl');
 INSERT INTO Meals VALUES(4002, 1.50, 'Il Forno', 'Extra Chicken');
@@ -70,7 +71,7 @@ CREATE VIEW ActiveRestaurants as (
 SELECT location, count(*)
 From Activeseller
 GROUP BY location
-ORDER BY location);
+ORDER BY count(*) DESC);
 
 CREATE ROLE web_anon nologin;
 
